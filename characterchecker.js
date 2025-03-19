b2a740fdbebba6b98a78489804fa76df90df6b76
Nate <wtwildnateis@gmail.com>
2025-03-18 20:33:23 -0500

    Character checker practice for Launchcode

--- characterchecker.js
+++ characterchecker.js
@@ -3,4 +3,4 @@ let response = input.question("Welcome, will you please enter the first word tha
 console.log("You entered " + response + "! This word contains " + response.length + " characters! "); // Outputs the word the user entered and displays the number of characters in the word.
 let question1 = input.questionInt("Let's pick a random index and find it shall we?! Starting at 0, pick a number 0 through " + [response.length-1] + "! "); // Asks user to pick a number between 0 and the number of character in the word they chose
 console.log("The index of " + question1 + " in " + response + " is " + response[question1] + " !"); // Outputs the index the user chose and the character at that index in the word they chose.");
-console.log("Thanks for participating! Now I'm gonna go and... I dunno process something I guess. K, Bye!");
+console.log("Thanks for participating! Now I'm gonna go and... I dunno process something I guess. K thanks, Bye!");
